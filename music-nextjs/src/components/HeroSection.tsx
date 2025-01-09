af79f8bb3a07bded5bcb909c6f862969b689b2e2
Aman Kumar <amankarguwal0@gmail.com>
2025-01-09 18:27:37 +0530

    fix: adjust HeroSection height for better layout consistency

--- music-nextjs/src/components/HeroSection.tsx
+++ music-nextjs/src/components/HeroSection.tsx
@@ -4,7 +4,7 @@ import { Spotlight } from "./ui/spotlight";
 
 export default function HeroSection() {
   return (
-    <div className="h-auto md:h-[60rem]  w-full rounded-md flex flex-col items-center justify-center relative overflow-hidden mx-auto py-10 md:py-0">
+    <div className="h-auto md:h-[59rem]  w-full rounded-md flex flex-col items-center justify-center relative overflow-hidden mx-auto py-10 md:py-0">
       <Spotlight
         className="-top-40 left-0 md:left-60 md:-top-20"
         fill="white"
