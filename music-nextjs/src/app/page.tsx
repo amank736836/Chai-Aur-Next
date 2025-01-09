24e06667189e7a8fe2246109c6c19ea657fb156a
Aman Kumar <amankarguwal0@gmail.com>
2025-01-09 19:32:13 +0530

    add UpcomingWebinars component and update background style for improved visibility

--- music-nextjs/src/app/page.tsx
+++ music-nextjs/src/app/page.tsx
@@ -1,16 +1,17 @@
 import FeaturedCourses from "@/components/FeaturedCourses";
 import HeroSection from "@/components/HeroSection";
 import MusicSchoolTestimonials from "@/components/TestimonialCards";
+import UpcomingWebinars from "@/components/UpcomingWebinars";
 import WhyChooseUs from "@/components/WhyChooseUs";
-import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
+    <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.05]">
       <HeroSection />
       <FeaturedCourses />
       <WhyChooseUs />
       <MusicSchoolTestimonials />
+      <UpcomingWebinars />
     </main>
   );
 }
