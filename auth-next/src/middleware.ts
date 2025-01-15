3e9acc04b66040578fc112fc30731e08093fe290
Aman Kumar <amankarguwal0@gmail.com>
2025-01-15 14:17:55 +0530

    feat: add middleware for authentication handling and public path redirection

---
+++ auth-next/src/middleware.ts
@@ -0,0 +1,24 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export function middleware(request: NextRequest) {
+  const path = request.nextUrl.pathname;
+
+  const isPublicPath =
+    path === "/login" || path === "/signup" || path === "/verify";
+
+  const token = request.cookies.get("token")?.value || "";
+
+  console.log("token", token);
+
+  if (isPublicPath && token) {
+    return NextResponse.redirect(new URL("/profile", request.nextUrl));
+  }
+
+  if (!isPublicPath && !token) {
+    return NextResponse.next();
+  }
+}
+
+export const config = {
+  matcher: ["/", "/login", "/signup", "/verify", "/profile", "/profile/:id"],
+};
