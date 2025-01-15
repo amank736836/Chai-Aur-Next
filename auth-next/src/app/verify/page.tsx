3ae4e9d56a3aa028af5513d9e79d0be15320a1de
Aman Kumar <amankarguwal0@gmail.com>
2025-01-15 10:50:52 +0530

    add email verification page with token handling and user feedback

---
+++ auth-next/src/app/verify/page.tsx
@@ -0,0 +1,87 @@
+"use client";
+
+import axios from "axios";
+import Link from "next/link";
+import { useRouter, useSearchParams } from "next/navigation";
+import { useEffect, useState } from "react";
+import toast from "react-hot-toast";
+
+function VerifyEmailPage() {
+  const [token, setToken] = useState("");
+
+  const [Verified, setVerified] = useState(false);
+
+  const [error, setError] = useState(false);
+
+  const router = useRouter();
+
+  const verifyUserEmail = async () => {
+    const toastId = toast.loading("Verifying email...");
+    try {
+      const response = await axios.post("/api/users/verify", { token });
+
+      console.log("Verified", response.data);
+
+      if (response.data.success) {
+        setVerified(true);
+        toast.success(response.data.message);
+        router.push("/login");
+      } else {
+        setError(true);
+        toast.error(response.data.message);
+      }
+    } catch (error) {
+      console.error(error);
+      setError(true);
+      toast.error("Error verifying email");
+    } finally {
+      toast.dismiss(toastId);
+    }
+  };
+
+  const params = useSearchParams();
+  useEffect(() => {
+    const Token = params.get("token") || "";
+
+    if (params.get("token")) {
+      setToken(Token);
+      console.log("Token", Token);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (token && token.length > 0) {
+      verifyUserEmail();
+    }
+  }, [token]);
+
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2 gap-4">
+      <h1 className="text-4xl">Verify Email</h1>
+
+      <h2 className="p-2 bg-orange-500 text-black">
+        {token && token.length > 0 ? (
+          <button onClick={verifyUserEmail}>Verify Email</button>
+        ) : (
+          "Invalid token"
+        )}
+      </h2>
+
+      {Verified && (
+        <div>
+          <h2>Email verified successfully</h2>
+          <Link href="/login">Login</Link>
+        </div>
+      )}
+
+      {error && (
+        <div>
+          <h2>Error verifying email</h2>
+          <Link href="/login">Login</Link>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default VerifyEmailPage;
